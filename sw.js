18bc5db85bced486fb578bec5b439fe39361d6aa
hemaallaqany <hemaallaqany@gmail.com>
2026-06-09 18:37:56 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'imposter-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// تثبيت الـ Service Worker وحفظ الملفات في الكاش
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// استدعاء الملفات من الكاش لتسريع اللعبة وتشغيلها بدون إنترنت
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
